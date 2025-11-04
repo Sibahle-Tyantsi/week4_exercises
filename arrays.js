0fc4c08511f72bd7a8a720238ecf11e10c245322
Sibahle-Tyantsi <222457244@mycput.ac.za>
2025-11-04 15:40:35 +0200

    first commit

---
+++ arrays.js
@@ -0,0 +1,18 @@
+// TODO: Create an empty array called colors
+let colors = [];
+// TODO: Add three colors to the array using push()
+colors.push("blue");
+colors.push("purple");
+colors.push("black");
+// TODO: Create another array called numbers with five numbers
+let numbers = [1,2,3,4.5]
+// TODO: Remove the last color from the colors array
+colors.pop()
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("pink")
+// TODO: Print the length of both arrays
+console.log("Colors array:", colors);
+console.log("Numbers array:", numbers);
+console.log("Length of colors array:", colors.length);
+console.log("Length of numbers array:", numbers.length);
+
---
+++ operations-types.js
@@ -0,0 +1,21 @@
+// TODO: Create a variable called number1 and store any number in it
+let num1 =10;
+
+// TODO: Create a variable called number2 and store a different number in it
+let num2 = 2;
+
+// TODO: Convert number1 to a string and store it in a new variable
+number1 = String(num1);
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+sum = num1+num2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+product = num1 * num2;
+
+// TODO: Print all results to the console
+console.log("This is my first my number = ", num1);
+console.log("This is my second my number = ", num2);
+console.log("Number 1 as a string is : ", number1);
+console.log("Sum = ", sum);
+console.log("Product = ", product);
